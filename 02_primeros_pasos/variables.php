5afbfe51462d06acb0df57a41ef1550a7d57ff4d
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2020-03-17 08:07:02 -0700

    Definir variables
    Indicaciones para definir variables

---
+++ 02_primeros_pasos/variables.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Nombres de variables
+ * 
+ * Deben iniciar con signo de $
+ * Seguido de una letra o _
+ * Pueden contener letras (alfabeto inglés), números y _
+ * Distingue mayúsculas y minúsculas (Case Sensitive)
+ * No se pueden utilizar palabras reservadas
+ * Se recomienda utilizar nombres que tengan sentido
+ */
+$nombre = 'Bidkar';
+$apellidos = 'Aragón Cárdenas';
+$edad = 40;
+$estaEnfermo = false;
+$habraClasesTomorrow = null;
+$estatura = 1.78;
+$correo_electronico = 'bidkar@cetis108.edu.mx';
+// $4semestre = 'Cool'; Incorrecto
+// $nombre completo = 'BAC';
