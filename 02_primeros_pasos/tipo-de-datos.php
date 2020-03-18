99f3057339558816f11a8d4b258eea22d6abb7ae
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2020-03-18 12:37:25 -0700

    Convertir tipos de datos
    Se describieron las formas de cambiar el tipo de dato a las variables

--- 02_primeros_pasos/tipo-de-datos.php
+++ 02_primeros_pasos/tipo-de-datos.php
@@ -18,3 +18,16 @@ echo '$booleano es de tipo: '.gettype($booleano)."\n";
 echo '$arreglo es de tipo: '.gettype($arreglo)."\n";
 echo '$objeto es de tipo: '.gettype($objeto)."\n";
 echo '$nulo es de tipo: '.gettype($nulo)."\n";
+
+$entero = "cadena";
+echo '$entero es de tipo: '.gettype($entero)."\n";
+settype($decimal, "string");
+echo '$decimal es de tipo: '.gettype($decimal)."\n";
+$cadena2 = "lfdsjflksdjl";
+settype($cadena2, "integer");
+echo '$cadena2 es de tipo: '.gettype($cadena2)."\n";
+echo $cadena2."\n";
+
+$num_cadena = "123"; // string
+$numerico = (integer) $num_cadena; // casting
+echo gettype($numerico);
